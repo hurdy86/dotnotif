04ad2dfdda7fd0c7b1fc45410193374769807fc9
hurdy86 <hdotsimpel@gmail.com>
2016-03-05 00:17:01 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,23 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-  }).catch(function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
-}
+window.onload = function() {  
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful, show interface
+      document.getElementById('not-status').innerHTML = '<h4>mobiForge notifications</h4><input type="checkbox" name="pushStatus" id="pushStatus" value="false" /><label for="pushStatus">Receive push notifications for new content</label><div id="pushStatusMsg"></div>';
+
+      //Check subscription state
+      checkSubscription();
+
+      //Attach listener
+      document.getElementById("pushStatus").addEventListener('click', function(){
+        subUnsubPush();
+      });
+    }).catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  }
+  else {
+    console.log("ServiceWorker not supported :-(");
+    document.getElementById('not-status').innerHTML = 'ServiceWorker not supported :-(';
+  }  
+};
